7092a6a82aa3d879a2b16fd4ab82855ec30da5c7
xuhe <xuhe114514@gmail.com>
2024-09-01 23:14:58 +0800

    feat: init repo and add remote repo

--- network/repo.go
+++ network/repo.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
 )
 
 type RepoConfig struct {
@@ -30,6 +31,37 @@ func (r *Repo) Download() error {
 	return nil
 }
 
+func (r *Repo) Init() error {
+	// init repo
+	if _, err := git.PlainInit(r.Path, false); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Repo) AddRemote() error {
+	repo, err := git.PlainOpen(r.Path)
+	if err != nil {
+		return err
+	}
+	_, err = repo.CreateRemote(&config.RemoteConfig{
+		Name: "origin",
+		URLs: []string{r.Url},
+	})
+	return err
+}
+
 func (r *Repo) Upload() error {
+	// init repo if the repo is not initialized
+	if _, err := git.PlainOpen(r.Path); err != nil {
+		// init repo
+		if err := r.Init(); err != nil {
+			return err
+		}
+	}
+	// add remote if the remote is not added
+	if err := r.AddRemote(); err != nil {
+		return err
+	}
 	return nil
 }
